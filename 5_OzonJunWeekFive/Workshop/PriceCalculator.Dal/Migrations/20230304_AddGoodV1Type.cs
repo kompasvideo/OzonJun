c0cb11a77d721277e8d68778797d9a2b41ed9334
Андрей Барышников <andrey1andrey@narod.ru>
2024-06-14 23:20:47 +0300

    2_30

---
+++ 5_OzonJunWeekFive/Workshop/PriceCalculator.Dal/Migrations/20230304_AddGoodV1Type.cs
@@ -0,0 +1,40 @@
+﻿using FluentMigrator;
+
+namespace PriceCalculator.Dal.Migrations;
+
+public class AddGoodTypeV1 : Migration
+{
+    public override void Up()
+    {
+        const string sql = @"
+DO $$
+    BEGIN
+        IF NOT EXISTS (SELECT 1 FROM pg_type WHERE typname = 'goods_v1') THEN
+            CREATE TYPE goods_v1 as 
+            (
+                  id            bigint
+                , user_id       bigint
+                , width         double precision
+                , height        double precision
+                , length        double precision
+                , weight        double precision
+            );
+        END IF;
+    END
+$$;";
+        
+        Execute.Sql(sql);
+    }
+
+    public override void Down()
+    {
+        const string sql  =  @"
+DO  $$
+    BEGIN
+        DROP TYPE IF EXISTS goods_v1;
+    END
+$$;";
+        
+        Execute.Sql(sql);
+    }
+}
---
+++ 5_OzonJunWeekFive/Workshop/PriceCalculator.Dal/Repositories/Interfaces/IDbRepository.cs
@@ -0,0 +1,8 @@
+﻿using System.Transactions;
+
+namespace PriceCalculator.Dal.Repositories.Interfaces;
+
+public interface IDbRepository
+{
+    TransactionScope CreateTransactionScope(IsolationLevel level = IsolationLevel.ReadCommitted);
+}

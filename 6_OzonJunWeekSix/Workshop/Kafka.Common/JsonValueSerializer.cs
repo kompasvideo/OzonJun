eda73f684b21cadacc1ea36e73e43abbe8b2681b
Андрей Барышников <andrey1andrey@narod.ru>
2024-07-25 01:43:31 +0300

    1_01

---
+++ 6_OzonJunWeekSix/Workshop/Kafka.Common/JsonValueSerializer.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json;
+using Confluent.Kafka;
+
+namespace Kafka.Common;
+
+public sealed class JsonValueSerializer<T> :ISerializer<T>, IDeserializer<T>
+{
+    public byte[] Serialize(T data, SerializationContext context) 
+        => JsonSerializer.SerializeToUtf8Bytes(data);
+
+    public T Deserialize(ReadOnlySpan<byte> data, bool isNull, SerializationContext context)
+    {
+       if (isNull)
+           throw new ArgumentNullException(nameof(data), "Null data encountered");
+       return JsonSerializer.Deserialize<T>(data) 
+           ?? throw new ArgumentNullException(nameof(data), "Null data encountered");
+    }
+}
